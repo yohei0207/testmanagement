0144b6ae4994108a86cfc85b38772d23ad330fa3
yohei <yohei_green@cloud.com>
2020-08-27 23:20:07 +0900

    1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root "users#login"
+  root "users#login_form"
   get 'master/index' => "master#index"
   get 'shift/:id/register' => "shift#register"
   get "shift/:id/check" => "shift#check"
